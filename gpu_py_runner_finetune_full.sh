66e1c9c5655c0475526fd872bfc2c9137aba57d8
Hamed Ghazikhani <h_ghazik@viryasubmit.encs.concordia.ca>
2023-03-29 18:41:42 -0400

    grid

--- gpu_py_runner_finetune_full.sh
+++ gpu_py_runner_finetune_full.sh
@@ -3,14 +3,14 @@
 #$ -N cnncv
 #$ -cwd
 #$ -m bea
-#$ -l m_mem_free=32G,g=4
+#$ -l m_mem_free=64G,g=1
 
 export TMPDIR=~/tmp
 export TRANSFORMERS_CACHE=~/tmp
 
 source ~/python_venv/bin/activate
 
-python -m torch.distributed.launch --nproc_per_node=4 cnn_cv.py 
+python cnn_cv.py 
 
 deactivate
 
